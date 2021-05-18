8488a004379dccce366861f07d90ffc8252bf7c1
arturr92 <gonart.92@gmail.com>
2021-05-18 20:55:42 -0300

    ejercicio1

--- jsEventLoop/ejercicio_2.js
+++ jsEventLoop/ejercicio_2.js
@@ -1,7 +1,18 @@
 // que va a imprimir?
 // como lograr que imprima 1, 2, 3
-for (var i = 0; i < 3; i++) {
+// for (var i = 0; i < 3; i++) {
+//   setTimeout(function() {
+//     console.log(i)
+//   }, 1000 * i)
+// }
+
+for (var i = 1; i <= 3; i++) (function(i) {
   setTimeout(function() {
-    console.log(i)
-  }, 1000 * i)
-}
+      console.log(i)
+  }, 1000);
+})(i);
+
+
+
+
+
