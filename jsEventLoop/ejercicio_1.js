f9f61192199eca1e99d09d6097260701d311bd44
jgastrell <gastrelljonathan@gmail.com>
2021-05-18 15:41:59 -0300

    resolucion ej 1

--- jsEventLoop/ejercicio_1.js
+++ jsEventLoop/ejercicio_1.js
@@ -5,3 +5,17 @@ setTimeout(() => {
     console.log('second')
 },0)
 console.log('third')
+
+
+// console.log('first')
+
+// new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(console.log('second'))
+//   },0)
+// }).then(() => {
+//   console.log('third')
+// })
+
+
+
