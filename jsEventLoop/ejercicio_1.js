0060e65a7b3f13c2ebbb13270be6dfd0b6a0cebf
arturr92 <gonart.92@gmail.com>
2021-05-18 16:01:31 -0300

    Ejercio1

--- jsEventLoop/ejercicio_1.js
+++ jsEventLoop/ejercicio_1.js
@@ -1,2 +1,12 @@
 // como se imprime?
 // como lograr q se imprima en el orden correcto?
+console.log('first')
+new Promise( (resolve, reject) => {
+    setTimeout(() => {
+        resolve(console.log('second'));
+    },0)        
+})
+.then( () => console.log('third'))
+
+
+
