b40ca2d40f88dd102de99d079a562f92a95f9a42
jgastrell <gastrelljonathan@gmail.com>
2021-05-19 14:54:50 -0300

    resolucion

--- jsEventLoop/ejercicio_2.js
+++ jsEventLoop/ejercicio_2.js
@@ -1,7 +1,26 @@
 // que va a imprimir?
 // como lograr que imprima 1, 2, 3
-for (var i = 0; i < 3; i++) {
-  setTimeout(function() {
-    console.log(i)
-  }, 1000 * i)
+// for (var i = 0; i < 3; i++) {
+//   setTimeout(function() {
+//     console.log(i)
+//   }, 1000 * i)
+// }
+
+const function1 = () => {
+  const a = 2;
+  console.log(a)
+};
+
+const function2 = () => {
+  function1();
+  const b =3;
+  console.log(b)
 }
+
+const function3 = () => {
+  function2();
+}
+
+function3();
+
+
