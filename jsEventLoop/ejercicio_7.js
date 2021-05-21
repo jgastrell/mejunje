1df230699d95ab57e77723d78ef552ab51b97d19
arturr92 <gonart.92@gmail.com>
2021-05-21 12:58:00 -0300

    resolucion

--- jsEventLoop/ejercicio_7.js
+++ jsEventLoop/ejercicio_7.js
@@ -1,14 +1,14 @@
 (function sa() {
-  var fruits = {
-      color: 'yellow',
+    var fruits = {
+        color: 'yellow',
 
-      getColor() {
-          console.log(this.color)
-      }
-  }
+        getColor() {
+        console.log(this.color)
+    }
+}
 
-  function getFruitColor(color) {
-      console.log(fruits.color)
+    function getFruitColor(color) {
+        console.log(fruits.color)
   } //como hago para obtener el color sin pasar parametros
 })()
 
